f0a7404419bb8b29b9f59a9010148bd4778b884b
bloecher <bloechrr@gmail.com>
2023-08-14 15:16:06 +0200

    move player functions to player.py

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from player import Player
 import random 
 
 MAX_LINES = 3
+
 MAX_BET= 100
 MIN_BET=1
 
@@ -44,31 +45,6 @@ def print_slot_machine(columns):
             else:
                 print(column[row])
 
-def hello():
-    #Hello welcomes the player/user to the game. It asks for the players name and asks them to confirm with a (y/n) 
-    name = input("\nWELCOME TO OBELICKS' SLOT MACHINE\nWhat is your name? \n")
-
-    yn = input(f"your name is {name}. Is that correct (y/n)")
-    while yn in ["n", "N"]:
-        name = input("Sorry, lets try that again!\n What is your name?")
-        
-        yn = input(f"your name is {name}. Is that correct (y/n)")
-    return name
-
-def deposit():
-    #allows the user to enter the number of money they want to deposit for the game
-    #The code checks if the input is a number greater than 0 before accepting the amount depositet
-    while True:
-        amount = input("How much money would you like to deposit? $")
-        if amount.isdigit():
-            amount= int(amount)
-            if amount > 0:
-                break
-            else:
-                print("Must be greater than 0.")
-        else: 
-            print("Invalid input. Only whole numbers are allowed!")
-    return amount
 
 def get_number_of_lines():
     #allows the user to enter the number of money they want to deposit for the game
@@ -103,11 +79,11 @@ def place_bet():
 
 def main():
     #Getting Player Information    
-    name = hello()
-    balance = deposit()
+    # LEGACY name = hello()
+    # LEGACY balance = deposit()
 
     #Making the Player Class see player.py
-    player = Player(name,balance)
+    player = Player()
     player.show()
 
     #getting bet information
@@ -125,4 +101,4 @@ def main():
     slots = get_slot_machine_spin(ROWS,COLS,symbol_count)
     print_slot_machine(slots)
 
-main()
+main()  
--- player.py
+++ player.py
@@ -1,7 +1,7 @@
 class Player:
-    def __init__(self,name,balance) -> None:
-        self.name = name
-        self.balance = balance
+    def __init__(self) -> None:
+        self.name = hello()
+        self.balance = deposit()
 
     def show(self):
         print(f"Player: {self.name} has ${self.balance} on his account")
@@ -14,3 +14,30 @@ class Player:
             return False
         else:
             return True
+
+def deposit():
+    #allows the user to enter the number of money they want to deposit for the game
+    #The code checks if the input is a number greater than 0 before accepting the amount depositet
+    while True:
+        amount = input("How much money would you like to deposit? $")
+        if amount.isdigit():
+            amount= int(amount)
+            if amount > 0:
+                break
+            else:
+                print("Must be greater than 0.")
+        else: 
+            print("Invalid input. Only whole numbers are allowed!")
+    return amount
+
+def hello():
+    #Hello welcomes the player/user to the game. It asks for the players name and asks them to confirm with a (y/n) 
+    name = input("\nWELCOME TO OBELICKS' SLOT MACHINE\nWhat is your name? \n")
+
+    yn = input(f"your name is {name}. Is that correct (y/n)")
+    while yn in ["n", "N"]:
+        name = input("Sorry, lets try that again!\n What is your name?")
+        
+        yn = input(f"your name is {name}. Is that correct (y/n)")
+    return name
+
